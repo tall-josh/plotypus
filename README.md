a575fe98ecff3f163d72fd0c2eb481eb9c70cd42
joshua patterson <joshua.patterson@silverpond.com.au>
2020-05-01 09:32:30 +1000

    moved chart code to different repo

--- README.md
+++ README.md
@@ -1,58 +1,24 @@
-# Metrics SVG
-
-The ultimate goal for this is to emulate teh GitHub Shield's functionality 
-but for simple charts This is what I'm working towards.
-
-Say I have a ML model with a metrics file.
-
-```yaml
-# training-loss.yaml
-
-run-01:
-  - 0.1
-  - 0.2
-  - 0.3
-  - 0.4
-run-02:
-  - 0.2
-  - 0.3
-  - 0.4
-  - 0.5
-```
-
-Well I would love to be able to reference this in my README.md
+# Usage
 
 ```
-![train loss](http://metrics-svg/line-chart/tall-josh/metrics-svg/blob/master/test/train-acc.yaml)
+pip install -r requirements.txt
+python launch.py
 ```
 
-And have it render a nice chart for me.
+# Markdown
 
-We could even add some additional keys to the `yaml`
+`![test](http://127.0.0.1:5000/get-chart/tall-josh/yamgal/master/examples/lines-01.yaml)`
+![test](http://127.0.0.1:5000/get-chart/tall-josh/yamgal/master/examples/lines-01.yaml)
 
-```yaml
-title: Train Accuracy
-x_title: Accuracy
-y_title: Poop
-x_labels: [a,b,c,d,e,f,g]
-y_labels: [a,b,c,d,e,f,g,h,i]
 
-# Valid values: cubic, quadratic, lagrange, trigonometric, hermite
-# or ommit from yaml
-# See: http://www.pygal.org/en/stable/documentation/configuration/interpolations.html
-#interpolate: lagrange
-data:
-    run-01:
-        - 0.1959999918937683
-        - 0.1069999814033508
-        - 0.1539999723434448
-        - 0.1889999985694885
-        - 0.1999999761581421
-        - 0.1160000085830688
-        - 0.1340000152587891
-...
-```
+# Gotcha
 
-This is a pre-baked example. Using Pygal. 
+Browsers tend to cache the images, so if your plat changes you can
+add a random `?<random strnig>` at the end of the url.
 
-![train metrics](https://k3l1upy16j.execute-api.ap-southeast-2.amazonaws.com/dev/line-chart/tall-josh/metrics-svg/master/test/train-acc_run-06.yaml)
+`![test](http://127.0.0.1:5000/get-chart/tall-josh/yamgal/master/examples/lines-01.yaml?<change this each time the plot changes>)`
+
+# ToDo
+
+  - [ ] Githook the does the finds image urls ending in `.yaml`. Taking hash of file and appending it to the url string.
+
--- launch.py
+++ launch.py
@@ -1,51 +1,23 @@
-import pygal
-from string import capwords
+from yamgal.make_chart import make_chart
 from flask import Flask, redirect, send_file
 from io import BytesIO
 import urllib3
 from uuid import uuid4
 from pathlib import Path
 import yaml
+import logging as log
+
+log.basicConfig(level=log.DEBUG)
 
 app = Flask(__name__)
 
 RAW_GITHUB_PREFIX='https://raw.githubusercontent.com/'
-CHART_TYPE='line-chart/'
+GET_CHART='get-chart/'
+CHART_TYPE='chart_type'
+ERROR_IMAGE='error.png'
 
 EXT='.svg'
 
-def make_chart(data):
-    #with open(data_yaml, 'r') as f:
-    #    data = yaml.load(f,  Loader=yaml.FullLoader)
-    assert 'data' in data, 'Must have at least "data" key'
-
-    # Default title
-    if not('title' in data):
-        data['title'] = capwords(Path(data_yaml)\
-                                       .stem\
-                                       .replace('-', ' ')\
-                                       .replace('_', ' '))
-
-    line_chart = pygal.Line(x_label_rotation=0,
-                            title   = data['title'],
-                            x_title = data.get('x_title',''),
-                            y_title = data.get('y_title',''),
-                            interpolate = data.get('interpolate', None),
-                            human_readable = True,
-                            width = data.get('width', 400),
-                            height = data.get('height', 300),
-                           )
-    x_count = 0
-    for run, values in data['data'].items():
-        line_chart.add(run, values)
-        if len(values) > x_count:
-            x_count = len(values)
-
-    line_chart.x_labels = data.get('x_labels', range(x_count))
-    line_chart.y_labels = data.get('y_labels', [])
-
-    #path = img_name + '_' + str(str(uuid4())) + ".svg"
-    return line_chart
 
 def get_yaml(url):
     http = urllib3.PoolManager()
@@ -54,16 +26,30 @@ def get_yaml(url):
     return yaml.load(data, Loader=yaml.FullLoader)
 
 def get_url(text):
-    i = text.index(CHART_TYPE)
-    loc = text[i+len(CHART_TYPE):]
+    i = text.index(GET_CHART)
+    loc = text[i+len(GET_CHART):]
     return f'{RAW_GITHUB_PREFIX}{loc}'
+
+def read_image_as_bytes(path):
+    with open(path, 'rb') as f:
+        image = f.read()
+    return BytesIO(image)
+
+def invalid_chart_type_error(data):
+    message = f'invalid chart type: "{data[chart_type]}"'
+    log.error(message)
+    return read_image_as_bytes('error.png')
+
+def generic_error(message):
+    log.error(message)
+    return read_image_as_bytes('error.png')
 
 @app.route('/<path:text>')
 def hello(text):
-    print(f'text: {text}')
-    if text.startswith(CHART_TYPE) and text.endswith('.yaml'):
+    log.info(f'text: {text}')
+    if text.startswith(GET_CHART) and text.endswith('.yaml'):
         url = get_url(text)
-        print(f'downloading: {url}')
+        log.info(f'downloading: {url}')
         yml = get_yaml(url)
         chart = make_chart(yml)
         image = BytesIO(chart.render())
@@ -71,7 +57,8 @@ def hello(text):
         filename = f'{Path(text).name}_{str(uuid4())}{EXT}'
         return send_file(image, attachment_filename=filename)
 
-    return 'Womp womp ¯\_(ツ)_/¯'
+    message = f'Invalid url error: "{text}"'
+    return send_file(generic_error(message), attachment_filename='error.png')
 
 
 if __name__ == '__main__':
